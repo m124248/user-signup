d7aa9ea4d1c623aa08e941c8009466e5800d415d
Catherine L <cluning08@gmail.com>
2018-04-08 15:56:36 -0500

    sunday afternoon commit

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 @app.route("/")
 def index():
     return render_template('homepage.html')
+
 
 def is_blank(str):
     if str == []:
         return True
 
-@app.route('/', methods=['POST'])
-def validate_input(): 
-    username = request.form['username']    
+
+@app.route('/signup', methods=['POST'])
+def validate_input():
+    username = request.form['username']
     password = request.form['password']
-    verifypass = request.form['verifypass']
+    verifypass = request.form['verify']
     email = request.form['email']
 
     username_error = ''
@@ -23,56 +26,52 @@ def validate_input():
     verifypass_error = ''
     email_error = ''
 
-    if not is_blank(username):
+    if is_blank(username):
         username_error = 'Not a valid username'
         username = ''
     else:
-        username = ''
-        if len(username) > 20 or len(password) <3:
+        if len(username) > 20 or len(username) < 3:
             username_error = 'Username lenth out of range (3-30)'
-            uername = ''
+            username = ''
 
-    if not is_blank(password):
+    if is_blank(password):
         password_error = 'Not a valid password'
         password = ''
-    else: 
-        password = ''
+    else:
         if len(password) > 20 or len(password) < 3:
             password_error = 'Password length out of range (3-20)'
             password = ''
-    
+
     if verifypass != password:
         verifypass_error = 'Password does not match!'
         verifypass = ''
-    else:
-        verifypass = ''
 
-    if not is_blank (email):
-        if '@' not in email and '.' not in email and '' in email: 
-            email_error = 'Not a valid email address'
-            email = ''
-    
-        else: 
-            email = ''
-            if len(email) > 20 or len(email) < 3:
-                email_error = 'email length out of range (3-20)'
-                email = ''
-    
+    # if is_blank(email):
+    #     email = ''
+    # else:
+    #     if not "@" or not "." or " " in email:
+    #         email_error = 'Not a valid email address'
+    #         email = ''
+    #     elif len(email) > 20 or len(email) < 3:
+    #         email_error = 'email length out of range (3-20)'
+    #         email = ''
+
     if not username_error and not password_error and not verifypass_error and not email_error:
-        username = ''
         return redirect('/welcome?username={0}'.format(username))
-    
     else:
-        return render_template('homepage.html', 
-            username_error=username_error, 
-            email_error=email_error,
-            username=username,
-            email=email)
-            
+        return render_template('homepage.html',
+                               username_error=username_error,
+                               email_error=email_error,
+                               password_error=password_error,
+                               verifypass_error=verifypass_error,
+                               username=username,
+                               email=email)
 
-@app.route("/welcome", methods=['POST'])
+
+@app.route("/welcome")
 def welcome():
-    username = request.form['username']
+    username = request.args.get('username')
     return render_template('welcomepage.html', name=username)
+
 
 app.run()
